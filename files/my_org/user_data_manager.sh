7178afcf1dd68361e6c195a3b2c12a4587800d04
Edward Herbert <eherbert@bgea.org>
2017-12-16 10:37:09 -0500

    All I got.

---
+++ files/my_org/user_data_manager.sh
@@ -0,0 +1,13 @@
+#cloud-boothook
+#!/bin/bash
+
+sleep 120
+
+nodeIp=$(ip addr | grep 'dynamic eth0' | awk -F 'inet ' '{print $2}' | awk -F '/25' '{print $1}')
+
+# Use postman to generate authorization token for you
+curl --request POST \
+   --url 'http://<JENKINS_URL>/job/AWS-Automation/job/Build%20AWS%20Swarm/job/master/build?delay=0sec' \
+   --header 'authorization: Basic <POSTMAN_AUTH_TOKEN>' \
+   --header 'cache-control: no-cache' \
+   --data-urlencode json="{'parameter': [{'name': 'nodeIp', 'value':'${nodeIp}'}, {'name': 'job', 'value': 'manager'}, {'name': 'org', 'value': '<ORG_PREFIX>'}]}"
